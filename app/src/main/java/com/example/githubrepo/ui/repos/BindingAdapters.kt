9d617bd6abc3d6bf2df8758330e28f7dba74a5d2
akrushinskiy <a.krushinskiy@evrius.com>
2019-12-27 19:14:09 +0200

    SimpleDateFormat

--- app/src/main/java/com/example/githubrepo/ui/repos/BindingAdapters.kt
+++ app/src/main/java/com/example/githubrepo/ui/repos/BindingAdapters.kt
@@ -1,11 +1,14 @@
 package com.example.githubrepo.ui.repos
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubrepo.models.Repo
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 @BindingAdapter("listData")
@@ -31,5 +34,17 @@ fun bindingId(textView: TextView, textId: Int?) {
 @BindingAdapter("repoString")
 fun bindingString(textView: TextView, text: String?) {
     textView.text = text
+}
+
+@SuppressLint("SimpleDateFormat")
+@BindingAdapter("repoDate")
+fun bindingDate(textView: TextView, text: String?) {
+    val dateformatGitHab = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+    val date = dateformatGitHab.parse(text)
+
+    val dateFormat = SimpleDateFormat("yyyy/mm/dd hh:mm:ss")
+    val strDate: String = dateFormat.format(date)
+
+    textView.text = strDate
 }
 
